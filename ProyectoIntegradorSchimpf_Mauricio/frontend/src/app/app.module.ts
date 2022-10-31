57a0849e7fb993cc9cee48db3911e4c7e9fc2b67
Mauricio Schimpf <mauri37128@gmail.com>
2022-10-31 06:17:15 -0300

    Cambios

--- ProyectoIntegradorSchimpf_Mauricio/frontend/src/app/app.module.ts
+++ ProyectoIntegradorSchimpf_Mauricio/frontend/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +41,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     NewExperienciaComponent,
     EditExperienciaComponent,
     NeweducacionComponent,
-    EditeducacionComponent
+    EditeducacionComponent,
+    EditSkillComponent,
+    NewSkillComponent
   ],
   imports: [
     BrowserModule,
